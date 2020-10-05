dee4d3631bbf4860357a8342be32a13ba2c3533f
Devin Cooper <dcoope12@asu.edu>
2020-10-05 01:35:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,15 +1,19 @@
 class Song
   attr_accessor :name, :artist
 
+@@all = []
   def initialize(name)
     @name = name
+    @@all << self
   end
 
   def artist_name
-    if @artist == nil
-      return nil
+    if !@artist
     else
       @artist.name
     end
-  end
+end
+    def self.all
+      @@all
+    end
 end
