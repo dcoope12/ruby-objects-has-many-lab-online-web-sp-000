fac8610bb3adc3deaa9eb33ac37ae379b3a83d54
Devin Cooper <dcoope12@asu.edu>
2020-10-05 01:37:15 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,15 +1,22 @@
 class Post
 
   attr_accessor :title, :author
+
+  @all = []
+  
   def initialize(title)
     @title = title
+    @@all << self
   end
 
   def author_name
-    if @author == nil
-      return nil
+    if !@author 
     else
       @author.name
     end
+  end
+
+  def self.all
+   @@all
   end
 end
